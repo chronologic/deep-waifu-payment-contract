8030200602b6ffdec4e4476894bdc9f67abd85ac
Marcin <zolkiewski.marcin@gmail.com>
2021-09-13 18:23:48 +0200

    trying to make it work

--- programs/deep-waifu-payment-contract/src/lib.rs
+++ programs/deep-waifu-payment-contract/src/lib.rs
@@ -9,70 +9,72 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod deep_waifu_payment_contract {
 
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, beneficiary: Pubkey, price_lamports: u64, max_count: u16) -> ProgramResult {
-        let storage_account = &mut ctx.accounts.storage_account;
+    pub fn initialize(ctx: Context<Initialize>, price_lamports: u64, max_count: u16, _bump: u8) -> ProgramResult {
+        let my_pda = &mut ctx.accounts.my_pda;
 
-        if storage_account.count > 0 {
+        if my_pda.count > 0 {
             return Err(ErrorCode::AlreadyInitialized.into());
         }
 
-        storage_account.price_lamports = price_lamports;
-        storage_account.beneficiary = beneficiary;
-        storage_account.max_count = max_count;
-        storage_account.count = 1;
+        my_pda.price_lamports = price_lamports;
+        my_pda.beneficiary = *ctx.accounts.payer.key;
+        my_pda.max_count = max_count;
+        my_pda.count = 1;
         Ok(())
     }
 
-    pub fn pay_for_mint(ctx: Context<PayForMint>) -> ProgramResult {
-        let storage_account = &mut ctx.accounts.storage_account;
+    // pub fn pay_for_mint(ctx: Context<PayForMint>) -> ProgramResult {
+    //     let my_pda = &mut ctx.accounts.my_pda;
 
-        if storage_account.count == 0 {
-            return Err(ErrorCode::NotInitialized.into());
-        }
+    //     if my_pda.count == 0 {
+    //         return Err(ErrorCode::NotInitialized.into());
+    //     }
 
-        if ctx.accounts.beneficiary.key != &storage_account.beneficiary {
-            return Err(ErrorCode::InvalidBeneficiary.into());
-        }
+    //     if ctx.accounts.beneficiary.key != &my_pda.beneficiary {
+    //         return Err(ErrorCode::InvalidBeneficiary.into());
+    //     }
 
-        msg!("Transfer {} lamports to {}", storage_account.price_lamports, storage_account.beneficiary);
-        invoke(
-            &system_instruction::transfer(ctx.accounts.user.key, ctx.accounts.beneficiary.key, storage_account.price_lamports),
-            &[
-                ctx.accounts.user.clone(),
-                ctx.accounts.beneficiary.clone(),
-                ctx.accounts.system_program.clone(),
-            ],
-        )?;
+    //     msg!("Transfer {} lamports to {}", my_pda.price_lamports, my_pda.beneficiary);
+    //     invoke(
+    //         &system_instruction::transfer(ctx.accounts.user.key, ctx.accounts.beneficiary.key, my_pda.price_lamports),
+    //         &[
+    //             ctx.accounts.user.clone(),
+    //             ctx.accounts.beneficiary.clone(),
+    //             ctx.accounts.system_program.clone(),
+    //         ],
+    //     )?;
 
-        storage_account.count += 1;
-        msg!("New count is {}", storage_account.count);
-        Ok(())
-    }
+    //     my_pda.count += 1;
+    //     msg!("New count is {}", my_pda.count);
+    //     Ok(())
+    // }
 
 
 }
  
 #[derive(Accounts)]
+#[instruction(bump: u8)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 2 + 2 + 40)]
-    pub storage_account: Account<'info, PaymentStorage>,
+    // #[account(init, payer = payer, seeds = [b"payment-storage".as_ref()], bump = bump, space = 8 + 2 + 2 + 40)]
+    #[account(init, payer = payer, seeds = [b"payment-storage".as_ref()], bump = bump, space = 8 + 2 + 2 + 40)]
+    pub my_pda: Account<'info, PaymentStorage>,
     #[account(signer)]
-    pub user: AccountInfo<'info>, 
+    pub payer: AccountInfo<'info>,
     #[account(address = system_program::ID)]
     pub system_program: AccountInfo<'info>,
 }
 
-#[derive(Accounts)]
-pub struct PayForMint<'info> {
-    #[account(mut)]
-    pub storage_account: Account<'info, PaymentStorage>,
-    #[account(signer)]
-    pub user: AccountInfo<'info>,
-    #[account()]
-    pub beneficiary: AccountInfo<'info>,
-    #[account(address = system_program::ID)]
-    pub system_program: AccountInfo<'info>,
-}
+// #[derive(Accounts)]
+// pub struct PayForMint<'info> {
+//     #[account(mut, seeds = [b"payment-storage".as_ref()], bump)]
+//     pub my_pda: Account<'info, PaymentStorage>,
+//     #[account(signer)]
+//     pub payer: AccountInfo<'info>,
+//     #[account()]
+//     pub beneficiary: AccountInfo<'info>,
+//     #[account(address = system_program::ID)]
+//     pub system_program: AccountInfo<'info>,
+// }
 
 #[account]
 pub struct PaymentStorage {
--- tests/deep-waifu-payment-contract.js
+++ tests/deep-waifu-payment-contract.js
@@ -8,24 +8,42 @@ describe('deep-waifu-payment-contract', () => {
     // Add your test here.
     const program = anchor.workspace.DeepWaifuPaymentContract;
 
-    const storageAccount = anchor.web3.Keypair.generate();
+    // const myPda = anchor.web3.Keypair.generate();
     const beneficiary = anchor.web3.Keypair.generate();
+    // anchor.web3.Keypair.
+
+    // console.log(anchor.Provider.env());
 
     const provider = anchor.getProvider();
 
+    const [paymentStoragePda, paymentStorageBump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from(anchor.utils.bytes.utf8.encode('payment-storage'))],
+        program.programId
+      );
+
+    console.log({ paymentStoragePda, paymentStorageBump });
+
+    console.log('SIGNER', provider.wallet.publicKey.toBase58());
+    console.log('PROGRAM ID', program.programId.toBase58());
+    console.log('STORAGE PDA', paymentStoragePda.toBase58());
+
+    // console.log(provider.wallet.payer.publicKey.toBase58());
+
+    const priceLamports = new anchor.BN(12_345_678);
+    const maxCount = new anchor.BN(10_000);
     const tx = await program.rpc.initialize(
-      {
-        beneficiary: beneficiary.publicKey,
-        priceLamports: new anchor.BN(12_345_678),
-        maxCount: new anchor.BN(10_000),
-      },
+      priceLamports,
+      maxCount,
+      paymentStorageBump,
       {
         accounts: {
-          storageAccount: storageAccount.publicKey,
-          user: provider.wallet.publicKey,
+          myPda: paymentStoragePda,
+          payer: provider.wallet.publicKey,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
           systemProgram: anchor.web3.SystemProgram.programId,
         },
-        signers: [storageAccount],
+        // signers: [provider.wallet.payer],
       }
     );
     console.log('Your transaction signature', tx);
